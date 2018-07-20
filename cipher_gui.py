bb4a0884b8d10d70e2027d74a868f043d1c21d55
cozybonfire <gfruiz@ucsd.edu>
2018-07-20 06:20:53 +0000

    start adding GUI

---
+++ cipher_gui.py
@@ -0,0 +1,10 @@
+# File:        cipher_gui.py
+# Programmer:  Gabriel Ruiz - github.com/cozybonfire
+# Description: Cipher's non-console UI - GUI!
+
+from tkinter import *
+
+root = Tk()
+root.title("Cipher - A Cryptography Utility Program")
+root.geometry("800x600")
+root.mainloop()
